ac63dfe0840692430d83c1a5aaa198228d067f75
jinsugyeong <apr15th@naver.com>
2026-03-19 00:28:28 +0900

    fix: OAuth state 동적 생성으로 변경

--- api/auth.js
+++ api/auth.js
@@ -1,13 +1,24 @@
-const { AuthorizationCode } = require('simple-oauth2');
+// auth.js
+const crypto = require('crypto');
+
 module.exports = (req, res) => {
   const client = new AuthorizationCode({
     client: { id: process.env.OAUTH_CLIENT_ID, secret: process.env.OAUTH_CLIENT_SECRET },
-    auth: { tokenHost: 'https://github.com', tokenPath: '/login/oauth/access_token', authorizePath: '/login/oauth/authorize' }
+    auth: {
+      tokenHost: 'https://github.com',
+      tokenPath: '/login/oauth/access_token',
+      authorizePath: '/login/oauth/authorize'
+    }
   });
+
+  // state를 쿼리에서 받아서 그대로 넘겨줌
+  const state = req.query.state || crypto.randomBytes(16).toString('hex');
+
   const authorizationUri = client.authorizeURL({
     redirect_uri: `https://${req.headers.host}/api/callback`,
     scope: 'repo,user',
-    state: 'random_string'
+    state: state  // ← Decap이 보낸 state 그대로 사용
   });
+
   res.redirect(authorizationUri);
 };
